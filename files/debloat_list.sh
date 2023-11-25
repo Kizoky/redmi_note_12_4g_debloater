9b07bb4254cc6412cb939daeb91cf92660fbc2f6
Kizoky <kizoky07@protonmail.com>
2023-11-25 10:25:04 +0100

    Init repo

---
+++ files/debloat_list.sh
@@ -0,0 +1,40 @@
+#!/system/bin/sh
+
+UNINSTALL="pm uninstall -k --user 0"
+
+echo ""
+echo "Uninstalling apps..."
+echo ""
+# Assuming you have deleted everything by hand, but can't uninstall these...
+	${UNINSTALL} com.facebook.system
+	${UNINSTALL} com.mi.globalbrowser
+	${UNINSTALL} com.google.android.apps.youtube.music
+	${UNINSTALL} com.google.android.youtube
+	${UNINSTALL} com.altice.android.myapps
+	${UNINSTALL} com.mi.globalminusscreen
+	${UNINSTALL} com.xiaomi.mipicks
+	${UNINSTALL} com.google.android.apps.photos
+	${UNINSTALL} com.orange.update
+	${UNINSTALL} com.ironsource.appcloud.oobe.hutchison
+	${UNINSTALL} com.aura.oobe.deutsche
+	${UNINSTALL} de.telekom.tsc
+	${UNINSTALL} com.google.android.apps.googleassistant
+	${UNINSTALL} com.dti.bouyguestelecom
+	${UNINSTALL} com.android.chrome
+	${UNINSTALL} com.google.android.apps.restore
+	${UNINSTALL} com.google.android.apps.docs
+	${UNINSTALL} com.google.android.gms.supervision
+	${UNINSTALL} com.android.hotwordenrollment.xgoogle
+	${UNINSTALL} com.android.hotwordenrollment.okgoogle
+	${UNINSTALL} com.google.android.apps.subscriptions.red
+	${UNINSTALL} com.google.android.videos
+	${UNINSTALL} com.google.android.apps.tachyon
+	${UNINSTALL} com.facebook.services
+	${UNINSTALL} com.facebook.appmanager
+	${UNINSTALL} com.xiaomi.payment
+	${UNINSTALL} com.dti.telefonica
+	${UNINSTALL} com.altice.android.myapps
+	${UNINSTALL} com.sfr.android.sfrjeux
+	${UNINSTALL} com.orange.aura.oobe
+	${UNINSTALL} com.google.android.apps.safetyhub
+	${UNINSTALL} com.aura.oobe.vodafone
